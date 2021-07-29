1830d75afa88cbf480855a07978141191c20f5c8
Tech Nerds <kpriyesh46@gmail.comgit config --global user.email kpriyesh46@gmail.com>
2021-07-29 11:44:48 +0530

    Bank Test initial commit

---
+++ Bank1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Bank1
+{
+    public class Account
+    {
+        private double balance;
+        public double Balance
+        {
+            get { return balance; }
+        }
+
+        public Account()
+        {
+            this.balance = 0;
+        }
+
+        public Account(double balance)
+        {
+            this.balance = balance;
+        }
+
+        
+        public void addAmount(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("Please don't add negative amount!!");
+            }
+
+            balance += amount;
+        }
+
+        public void withdrawAmount(double amount)
+        {
+            if (amount > balance)
+            {
+                throw new ArgumentOutOfRangeException("Withdraw amount cannot be more than balance!!");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("Please don't withdraw negative amount!!");
+            }
+
+            balance -= amount;
+        }
+
+        public void TransferFundsTo(Account otherAccount, double amount)
+        {
+            if (otherAccount == null)
+            {
+                throw new ArgumentNullException("Account doesn't exist!!");
+            }
+
+            withdrawAmount(amount);
+            otherAccount.addAmount(amount);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Account(2000);
+            Account a2 = new Account();
+            a1.TransferFundsTo(a2,500);
+            Console.WriteLine(a1.Balance + " " + a2.Balance);
+        }
+    }
+}
---
+++ TestBank/UnitTest1.cs
@@ -0,0 +1,61 @@
+
+using Bank1;
+using NUnit.Framework;
+using System;
+
+namespace TestBank
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        public Account account = new Account(1000);
+        [Test]
+        public void Add_Amount()
+        {
+            account.addAmount(500);
+            Assert.AreEqual(1500, account.Balance);
+        }
+
+        [Test]
+        public void Add_Negative_Amount()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => account.addAmount(-500));
+        }
+
+        [Test]
+        public void Transfer_Amount()
+        {
+            var otherAccount = new Account();
+            account.TransferFundsTo(otherAccount, 500);
+            Assert.AreEqual(1000, account.Balance);
+            Assert.AreEqual(500, otherAccount.Balance);
+        }
+
+        [Test]
+        public void Transfer_To_Non_Existing_Account_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => account.TransferFundsTo(null, 2000));
+        }
+
+        [Test]
+        public void Withdraw_Amount()
+        {
+           
+            account.withdrawAmount(500);
+            Assert.AreEqual(500, account.Balance);
+        }
+
+        
+        [Test]
+        public void Withdraw_Negative_Amount()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => account.withdrawAmount(-500));
+        }
+
+        [Test]
+        public void Withdraw_Overflow()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => account.withdrawAmount(2000));
+        }
+    }
+}
